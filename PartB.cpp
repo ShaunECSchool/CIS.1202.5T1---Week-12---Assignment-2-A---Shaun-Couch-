edd4a988e939d81891b8e1b9252e86863eff6403
ShaunECSchool <shaun.couch@sinclair.edu>
2026-08-02 22:52:56 -0400

    Cleanup

--- PartB.cpp
+++ PartB.cpp
@@ -9,53 +9,25 @@ August/2/2026
 
 using namespace std;
 
-//Instructions-------------------------------------------
-
-/*
-Write templated functions to perform math on varying numeric data types.
-Use a source code management tool as you develop this program.  Commit changes frequently using descriptive commit messages.  Push your changes to GitHub or other publicly accessible location for your instructor to clone.
-Requirements
-    1. Read the tips in the next section.
-    2. Define functions named "half" that accept a single argument, and return a value of that same type.
-    3. For floating-point types (float, double), return half of the value passed in as argument.
-    4. For integer types (int), return half the value, rounded according to usual rounding rules (0.5 or higher, round up; otherwise round down.)
-    5. Build a driver in main to test these functions and display their output.
-    6. Functions must pass parameters and return values as needed, using only local variables.  Global variables are not allowed.
-    7. Use comments to document your program as you see fit.  Include comments at the beginning of your program with your name, the class (with section), and the date.
-    8. Submit the URL for your repository to the dropbox.  No screen shots are needed.
-*/
-
-
 //Function Prototypes-------------------------------------------
 
-template <class T> //Modular data type handler
-void output(const T & n);
-template <class T> 
-void swapper(T & a, T & b);
+template <class T>
+T half(const T & n);
 
 
 //Main-------------------------------------------
 
 int main()
 {
-    output (5);
-    output (19284.3232);
-    output ("Hello, world!");
+    cout << half(5) << endl; //test1
+    cout << half(5.0) << endl; //test2
     return 0;
 }
 
-//Functions-------------------------------------------
-    
-template <class T>
-void output(const T & n)
-{
-    cout << n << endl;
-}
+//Function--------------------------------------------
 
 template <class T> 
-void swapper(T & a, T & b)
+T half(const T & n)
 {
-    T temp = a;
-    a = b;
-    b = temp;
+    return n / 2;
 }
